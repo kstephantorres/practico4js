a9b7a5dfc64bcc8b869495215483f896a384abaa
K Stephan Torres <kevinstephantorres@gmail.com>
2023-11-16 21:28:29 -0300

    Se termino el punto 3

--- js/punto03.js
+++ js/punto03.js
@@ -28,5 +28,34 @@ class Rectagulo {
     }
 
 }
+const rectangulo = new Rectagulo(10, 15)
 
+const btnMostrarRec = document.getElementById('btnMostrarRec')
+const btnPerimetro = document.getElementById('btnPerimetro')
+const btnArea = document.getElementById('btnArea')
 
+btnMostrarRec.addEventListener('click',()=>{
+    alert(`Rectangulo \n-Alto: ${rectangulo.alto} \n-Ancho: ${rectangulo.ancho}`)
+})
+
+btnPerimetro.addEventListener('click',()=>{
+    alert(`Perímetro: ${rectangulo.perimetro()}`)
+})
+btnArea.addEventListener('click',()=>{
+    alert(`Área: ${rectangulo.area()}`)
+})
+
+// const rectangul
+
+// const btnCrearRec = document.getElementById('btnCrearRec')
+// const btnPerimetro = document.getElementById('btnPerimetro')
+// const btnArea = document.getElementById('btnArea')
+
+// const altoInput = document.getElementById('altoInput')
+// const anchoInput = document.getElementById('anchoInput')
+
+// btnCrearRec.addEventListener('click',()=>{
+//     if(altoInput.value && anchoInput.value ){
+        
+//     }
+// })
